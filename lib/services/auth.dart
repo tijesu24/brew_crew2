d9e3bde900c183c9fdc2263c75e7ad87bafba425
Tijesunimi Adebiyi <tijesu24@gmail.com>
2021-08-21 22:04:47 +0100

    first commit

---
+++ lib/services/auth.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in anon
+  Future signInAnon() async {
+    print('Hi');
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign in with email and password
+
+
+  // register with email and password
+
+  // sign out
+
+}
